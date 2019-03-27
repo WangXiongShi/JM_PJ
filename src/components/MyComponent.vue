176b5e4f002822e74189b467ea388de7c4f9fbd4
王雄师 <wangxiongshi@xiaohe.com>
2019-03-27 11:28:12 +0800

    子父组件传值

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div>{{msg}}</div>
+        <ul v-if="dataHas">
+            <li v-for="(item,index) in dataList" :key="index">{{item.name +'　'+ item.age}}</li>
+        </ul>
+        <input v-model="username" @change="setUser" type="text">
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            dataHas:true,
+            username:''
+        }
+    },
+    props:{
+        dataList:Array,
+        msg:String
+    },
+    methods: {
+        setUser(){
+            this.$emit('transferUser',this.username)
+        }
+    },
+}
+</script>
+<style lang="">
+    
+</style>
