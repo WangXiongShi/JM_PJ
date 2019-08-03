08b73cca7170a82b167e3319863c9b8de05b5bf7
wangxiongshi <wangxiongshi@xiaohe.com>
2019-08-03 11:45:36 +0800

    创建登录页

---
+++ src/components/login.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        用户名:
+        <input type="text">
+        密码:
+        <input type="password" name="" id="">
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        
+    },
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    watch: {
+
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+};
+</script>
+
+<style scoped lang="">
+
+</style>
